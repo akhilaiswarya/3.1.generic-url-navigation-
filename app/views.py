0da27539e9e527b0733b5831fb17df2b7b5229e4
akhilaiswarya <aishwarya1403ca@gmail.com>
2023-11-21 21:58:06 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def aishwarya(request):
+    return render(request,'aishwarya.html')
+
+def cherry(request):
+    return render(request,'cherry.html')
+
+def aishwarya2(request):
+    return render(request,'aishwarya2.html')
